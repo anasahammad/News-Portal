2ff58096b159c91c1be6fb0d74497bb82094b1a0
anasahammad <anasahammadsarker@gmail.com>
2024-03-01 11:48:07 +0600

    done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const loadAllCatagory = async () =>{
+    
     const response = await fetch(`https://openapi.programming-hero.com/api/news/categories`)
     const data = await response.json();
     const newsCatagory = data.data.news_category;
@@ -12,7 +13,7 @@ const loadAllCatagory = async () =>{
 
         const div = document.createElement('div');
         div.innerHTML = `
-        <button  class="btn ">${item.category_name}</button>
+        <button onclick="loadNews('${item.category_id}')" class="btn ">${item.category_name}</button>
         `
         catagoryContainer.appendChild(div);
 
@@ -21,70 +22,116 @@ const loadAllCatagory = async () =>{
 }
 
 
-const loadNews = async () =>{
-    const res = await fetch("https://openapi.programming-hero.com/api/news/category/01")
+const loadNews = async (id) =>{
+    spinner(spinner)
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
     const data = await res.json();
     const news = data.data;
     // console.log(news)
+    displayNews(news)
 
-    news.forEach((item) =>{
-        console.log(item)
-
-        const newsContainer = document.getElementById('news-container');
-        const div = document.createElement('div');
-        div.classList.add('card', 'lg:card-side', 'bg-base-100',' shadow-xl');
-        div.innerHTML = `
-        <!-- <figure>
-                    <img src="" alt="Album"/>
-                </figure>
-                <div class="card-body">
-                  <h2 class="card-title">The best fashion influencers to follow for sartorial inspiration</h2>
-                  <p>From our favourite UK influencers to the best missives from Milan and the coolest New Yorkers, read on some of the best fashion blogs out there, and for even more inspiration, do head to our separate black fashion influencer round-up.
-
-                    Fancy some shopping deals? Check out these amazing sales: Zara Black Friday, ASOS Black Friday, Missoma Black Friday and Gucci Black Friday...</p>
-
-
-                        <div class="flex justify-between items-center">
-
-                        
-                    <div class="flex items-center gap-2">
-                        <div class="avatar">
-                            <div class="w-12 rounded-full">
-                              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                            </div>
-                          </div>
-
-                          <div>
-                            <p>Jane Cooper</p>
-                            <p>Jan 10, 2022 </p>
-                          </div>
-                    </div>
-
-                    <div class="flex gap-2">
-                        <img src="images/carbon_view.svg" alt="">
-                        <p>1.5M</p>
-                    </div>
-
-                    <div>
-                        <div class="rating">
-                            <input type="radio" name="rating-2" class="mask mask-star-2 " checked />
-                            <input type="radio" name="rating-2" class="mask mask-star-2 "  />
-                            <input type="radio" name="rating-2" class="mask mask-star-2 " />
-                            <input type="radio" name="rating-2" class="mask mask-star-2 " />
-                            <input type="radio" name="rating-2" class="mask mask-star-2 " />
-                          </div>
-                    </div>
-
-                    <div>
-                        <img src="images/bi_arrow-right-short.svg" alt="">
-                    </div>
-        
-        `
-
-    })
+    
+    
    
 }
 
-loadNews();
+
+const displayNews = (news) =>{
+
+    
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
+    news.forEach((item) =>{
+        console.log(item)
+
+       
+      
+        const div = document.createElement('div');
+        
+        div.classList.add('card', 'lg:card-side', 'bg-base-100', 'shadow-xl', 'my-4');
+       
+        div.innerHTML =   `
+        <figure>
+        <img src="${item.image_url
+        }" alt="Album"/>
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">${item.title}</h2>
+      <p>${item.
+        details}</p>
+
+
+            <div class="flex justify-between items-center">
+
+            
+        <div class="flex items-center gap-2">
+            <div class="avatar">
+                <div class="w-12 rounded-full">
+                  <img src="${item.author.img
+                  }" />
+                </div>
+              </div>
+
+              <div>
+                <p>${item.author.name}
+                </p>
+                <p>${item.author.published_date} </p>
+              </div>
+        </div>
+
+        <div class="flex gap-2">
+            <img src="images/carbon_view.svg" alt="">
+            <p>${item.total_view}</p>
+        </div>
+
+        <div>
+            <div class="rating">
+                <input type="radio" name="rating-2" class="mask mask-star-2 " checked />
+                <input type="radio" name="rating-2" class="mask mask-star-2 "  />
+                <input type="radio" name="rating-2" class="mask mask-star-2 " />
+                <input type="radio" name="rating-2" class="mask mask-star-2 " />
+                <input type="radio" name="rating-2" class="mask mask-star-2 " />
+              </div>
+        </div>
+
+        <div>
+            <img src="images/bi_arrow-right-short.svg" alt="">
+        </div>
+    
+        
+        
+        `
+        
+        newsContainer.appendChild(div)
+    })
+    spinner(false);
+
+}
+
+
+const spinner = (isLoading)=>{
+    const loading = document.getElementById('loading');
+
+    if(isLoading){
+        loading.classList.remove("hidden");
+    }
+    else{
+        loading.classList.add("hidden");
+    }
+    
+
+}
+
+
+const handleSearch = () =>{
+        const inputField = document.getElementById('input-field').value;
+        if(inputField){
+            loadNews(value);
+        }
+        else{
+            alert("Please inter valid news catagory");
+        }
+}
+loadNews("01");
 
 loadAllCatagory();
